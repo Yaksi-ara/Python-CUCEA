62c5c18a85160ac1b64574c7caa2c95f4ed14e7e
Yaksiara Pérez <veronika.perez7966@alumnos.udg.mx>
2023-06-02 15:08:35 -0600

    Agregué 2 programas que sí funcionan :D

---
+++ 2_Programa.py
@@ -0,0 +1,10 @@
+Preciomanzanas:int = 5
+Cantidadmanzanas:int = 10
+# Soy un comentario de una línea
+"""
+Soy un comentario multilínea
+Se hizo lo de asignar 0 pq ya se creó la variable, lo de int nomas es para q los demás sepan q tipo de 
+datos va a almacenar, pero no es necesario
+"""
+print("Costo a pagar: ")
+print(Preciomanzanas * Cantidadmanzanas)
