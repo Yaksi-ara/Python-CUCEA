cbeb8e0e9d1aae825c3c66a332d86a988b61b902
Yaksiara Pérez <veronika.perez7966@alumnos.udg.mx>
2023-06-04 20:13:47 -0600

    Corrección programa

--- Pruebadolar.py
+++ Pruebadolar.py
@@ -1,10 +1,11 @@
-Conversion = input("¿Quieres convertir dólares a pesos? Responde 1 si quieres convertir de pesos a dólares o responde 2 si quieres convertir dólares a pesos: ")
-if Conversion == "1":
- Dolar = 17.56
+Conversion = input("Conversor: Responde 1 si quieres convertir de pesos a dólares o responde 2 si quieres convertir dólares a pesos: ")
+if Conversion == "1": #PESOS A DÓLARES
+ Cambio = 17.56
  PesoMX = (int(input("Ingresa cantidad en pesos: ")))
- EquivalenciaPaD = PesoMX / Dolar
+ EquivalenciaPaD = PesoMX / Cambio
  print("Esto equivale a:", round(EquivalenciaPaD, 2) , "dólares")
-elif Conversion == "2":
- PesoMX = 0.057
- EquivalenciaDaP = Dolar * PesoMX
+elif Conversion == "2": #DÓLARES A PESOS
+ Cambio = 17.56
+ Dolar = (int(input("Ingresa cantidad en dólares: ")))
+ EquivalenciaDaP = Dolar * Cambio
  print("Esto equivale a:", round(EquivalenciaDaP, 2) , "pesos mexicanos")
