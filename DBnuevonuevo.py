66b3419c2368a54e7d1ed0d4f9a366cb268ce272
Yaksiara Pérez <veronika.perez7966@alumnos.udg.mx>
2023-06-26 14:40:47 -0600

    Conexión de la DB

--- DBnuevonuevo.py
+++ DBnuevonuevo.py
@@ -1,27 +1,31 @@
 import mysql.connector
+
 opcionBaseDeDatos = 1
-if opcionBaseDeDatos == 1 :
-    #Forma local
-    host="localhost"
-    user="root"
-    passwd="a"
+
+if opcionBaseDeDatos == 1:
+    # Forma local
+    host = "localhost"
+    user = "root"
+    passwd = ""
     port = 3306
-elif opcionBaseDeDatos == 2 :
-    #Forma remota
-    host="142.44.163.242"
-    user="Alumno13"
-    passwd="AlumnoPython1@."
+elif opcionBaseDeDatos == 2:
+    # Forma remota
+    host = "142.44.163.242"
+    user = "Alumno13"
+    passwd = "AlumnoPython1@."
     port = 4000
 
+conexion = None  # Define la variable conexion antes del bloque try
 try:
-	conexion = mysql.connector.connect(
-    host=host,
-    user=user,
-    passwd=passwd,
-    port=port
-)
-	print("Conexión correcta")
+    conexion = mysql.connector.connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        port=port
+    )
+    print("Se estableció conexión")
 except mysql.connector.Error as err:
-	print("Ocurrió un error al conectar: ", err)
+    print("Ocurrió un error al establecer conexión: ", err)
 finally:
-    conexion.close()
+    if conexion is not None:
+        conexion.close()
